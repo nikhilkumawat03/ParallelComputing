0f404ef93e9b761ea863d72c04f479ff4715dc68
Nikhil Kumawat <nikhilkumawat03@gmail.com>
2021-12-06 15:13:25 +0530

    Matrix initialization

--- parallelLUD.cpp
+++ parallelLUD.cpp
@@ -6,22 +6,42 @@
 
 using namespace std;
 
+
+void luDecomposition(float **matrix, float **&lowerMatrix, float **&upperMatrix, int matrixSize){
+	
+}
+
 int main(){
 	int matrixSize = 0;	//Matrix Size
 	cout << "Enter size of the matrix: " << endl;
 	cin >> matrixSize;
-	cout << "Step 1: Random matrix of size " << matrixSize << "x" << matrixSize << " will be generated." << endl;
 	
-	//Matrix is taken as input first creating the matrix.
+	//Random matrix is created.
 	float **matrix = new float*[matrixSize];
+	float **lowerMatrix = new float*[matrixSize];
+	float **upperMatrix = new float*[matrixSize];
+	
+	//Dynamic matrix size allocation.
 	for (int i = 0; i < matrixSize; ++i){
 		matrix[i] = new float[matrixSize];
+		lowerMatrix[i] = new float[matrixSize];
+		upperMatrix[i] = new float[matrixSize];
 	}
+	
+	//Random matrix is created.
 	for (int i = 0; i < matrixSize; ++i){
 		for (int j = 0; j < matrixSize; ++j){
 			matrix[i][j] = rand() % 100;
 		}
 	}
-	cout << "Step 1.1: Random matrix is generated of size: " << sizeof(float)*matrixSize*matrixSize << "Bytes." << endl;
 	
+	//initializtion of lower matrix.
+	//Diagonal elements are set to 1.
+	for (int i = 0; i < matrixSize; ++i){
+		lowerMatrix[i][i] = 1;
+	}
+	
+	cout << "Step 1: Random matrix is successfully created of size: " << sizeof(float)*matrixSize*matrixSize << "Bytes." << endl;
+	
+	luDecomposition(matrix, lowerMatrix, upperMatrix, matrixSize);
 }
