123e4874b0fc45467098617e9a407e4e1bff527a
chandrakishorSingh <chandrakishors52@gmail.com>
2021-12-12 20:28:22 +0530

    Add utility functions and comments in serialLUD.cpp
    - created utils.cpp to contain all utility functions to be used in multiple files
    - created a sample input file to take input for a matrix

---
+++ utils.h
@@ -0,0 +1,16 @@
+//
+// Created by chandrakishorsingh on 12/8/21.
+//
+
+#include <vector>
+
+using namespace std;
+
+// prints the matrix entries
+void print_matrix(vector<vector<double>>& matrix);
+
+// reads the matrix entries in the file and returns a Matrix
+vector<vector<double>> read_matrix(ifstream& file);
+
+// returns a matrix with randomly generated entries
+vector<vector<double>> generate_random_matrix(int n);
